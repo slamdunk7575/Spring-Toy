fe0e94a62de793ea9a5544bdd7a92f7aede3da23
slamdunk7575 <slamdunk7575@gmail.com>
2021-03-19 23:58:45 +0900

    (feat) MenuRepository 생성

---
+++ src/main/java/me/study/aop/repository/MenuRepository.java
@@ -0,0 +1,9 @@
+package me.study.aop.repository;
+
+import me.study.aop.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+}
